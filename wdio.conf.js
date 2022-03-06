32c388ae884eb6ce25676c1ea6bb2c990974b17f
niccolas.cassel <niccolas.cassel@sequor.com.br>
2022-03-06 15:12:36 -0300

    adição de dependências para continuidade do curso de testes mobile

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,9 +1,12 @@
 const { join } = require('path')
+const allure = require('allure-commandline')
+const video = require('wdio-video-reporter');
 
 exports.config = {
     hostname: 'localhost',
     port: 4723,
     path: '/wd/hub',
+    //services: ['appium'],
     specs: [
         './test/specs/**/*.js'
     ],
@@ -19,5 +22,40 @@ exports.config = {
     waitForTimeout: 20000,
     mochaOpts: {
         timeout: 300000
+    },
+    reporters: [
+        'spec',
+        ['allure', {
+            outputDir: 'allure-results',
+            disableWebdriverStepsReporting: false,
+            disableWebdriverScreenshotsReporting: false,
+        }],
+        [video, {
+            saveAllVideos: true,       // If true, also saves videos for successful test cases
+            videoSlowdownMultiplier: 50, // Higher to get slower videos, lower for faster videos [Value 1-100]
+          }]
+    ],
+    onComplete: function() {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    },
+    afterStep: function (test, scenario, { error, duration, passed }) {
+        driver.takeScreenshot();
     }
 }
